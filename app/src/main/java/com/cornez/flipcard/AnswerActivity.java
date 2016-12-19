dbc618582c7e5703018354d33b92b2d2ac6e8921
raghid91 <raghid.nami@gmail.com>
2016-12-18 21:43:23 -0500

    Initial commit

---
+++ app/src/main/java/com/cornez/flipcard/AnswerActivity.java
@@ -0,0 +1,33 @@
+package com.cornez.flipcard;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class AnswerActivity  extends Activity {
+
+    protected void onCreate (Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        setContentView(R.layout.activity_answer);
+
+        Button questionBtn = (Button) findViewById(R.id.button);
+        questionBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent showQuestion = new Intent(AnswerActivity.this, QuestionActivity.class);
+                startActivity(showQuestion);
+            }
+        });
+
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        overridePendingTransition(R.anim.answer_in, R.anim.question_out);
+    }
+
+}
